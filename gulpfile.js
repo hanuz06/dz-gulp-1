e67d8b8d6f9529aa2fc6a5063d0ce98593169ca4
Andrey <lee1975uz@gmail.com>
2019-05-20 12:36:46 -0500

    removed sass related code lines. Less will be used instead.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var less = require('gulp-less');
-var sass = require('gulp-sass');
+var styles = require('gulp-less');
+//var sass = require('gulp-sass');
 
-gulp.task('server', ['sass'], function() {
+gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
     // gulp.watch('./app/less/**/*.less', ['less']);
-    gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch('./app/less/**/*.less', ['sass']);
 });
 
 gulp.task('less', function() {
@@ -19,11 +19,5 @@ gulp.task('less', function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task('sass', function() {
-    return gulp.src('./app/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./app/css'))
-    .pipe(browserSync.stream());
-});
 
 gulp.task('default', ['server']);
