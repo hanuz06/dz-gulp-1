f6b27e0bfec611b7efdb5304a534814b61619a3f
Andrey <lee1975uz@gmail.com>
2019-05-20 20:15:18 -0500

    Replaced "less" tasks for styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var styles = require('gulp-less');
-//var sass = require('gulp-sass');
+var less = require('gulp-less');
 
 gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    // gulp.watch('./app/less/**/*.less', ['less']);
-    gulp.watch('./app/less/**/*.less', ['sass']);
+    gulp.watch('./app/less/**/*.less', ['styles']);
+    
 });
 
-gulp.task('less', function() {
+gulp.task('styles', function() {
     return gulp.src('./app/less/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('./app/css'))
